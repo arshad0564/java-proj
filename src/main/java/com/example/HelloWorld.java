6b8bf753997a41cb7830b1bdcd66ed9f494edbcb
arshad0564 <syedarshad0564@gmail.com>
2023-06-20 20:29:32 +0530

    .

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("this is artifact testing and sending war file to s3");
+    }
+}
+
